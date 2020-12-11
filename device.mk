333ea6c6e28b0ad079670547d8f31dd8f8f64675
xyzuan <i.xyzuan@gmail.com>
2020-12-11 11:08:08 +0000

    overlay-ancient: define phoenix specs.

--- device.mk
+++ device.mk
@@ -119,6 +119,9 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
+    $(DT_PATH)/overlay \
+    $(DT_PATH)/overlay-ancient \
+    $(DT_PATH)/overlay-system
 
 # Overlays - override vendor ones
 PRODUCT_PACKAGES += \
